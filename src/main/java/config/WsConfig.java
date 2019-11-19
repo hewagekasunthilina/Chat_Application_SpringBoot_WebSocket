1a59ab064159e82f8292619d3736af21b1738c11
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-11-19 13:33:07 +0530

    Chat Application server side

---
+++ src/main/java/config/WsConfig.java
@@ -0,0 +1,22 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WsConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/javatech").withSockJS();
+    }
+
+    public void configurerMessageBroker(MessageBrokerRegistry registry){
+       registry.enableSimpleBroker("/topic");
+       registry.setApplicationDestinationPrefixes("/app");
+    }
+}
---
+++ src/main/java/model/Message.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Message {
+
+    private String content;
+    private String sender;
+    private MessageType;
+
+    public enum MessageType{
+        CHAT,LEAVE,JOIN
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
